bdf7d8238a25a665b55a11afc0678032bb459b26
matheusBBarni <brehm.matheus@hotmail.com>
2021-03-21 22:39:27 -0300

    Updated README with the instructions to run the project

--- README.md
+++ README.md
@@ -5,6 +5,13 @@
 - npm >= 6.14.4.
 - yarn =< 1.22.4 (yarn version 2 or greater will not work with this project).
 
+## How to run?
+1. We need to install all dependencies: `yarn install`.
+2. Generate a build: `yarn build`.
+3. Wait for the build to end and run the project: `yarn start`;
+
 ## Commands
 - Dev: `yarn dev` or `npm run dev`.
-- test: `yarn test` or `npm run test`.
+- Test: `yarn test` or `npm run test`.
+- Build: `yarn build` or `npm run build`.
+- Start: `yarn start` or `npm run start`.
--- src/services/FlightsService.ts
+++ src/services/FlightsService.ts
@@ -3,7 +3,7 @@ import { IFlight } from '../model/IFlight';
 
 class FlightsService {
 
-  private async getFlights (code: string): Promise<IFlight[] | null> {
+  private async getFlights (code: string): Promise<IFlight[] | string | null> {
     try {
       const { data } = await api.get(`/${code}`);
       return data;
@@ -12,12 +12,17 @@ class FlightsService {
     }
   }
 
-  async getLatamFlights() {
-    return await this.getFlights('faadd728-2e31-42f9-bf69-fc245ff2867f');
+  async getLatamFlights(): Promise<IFlight[] | null> {
+    const result = await this.getFlights('faadd728-2e31-42f9-bf69-fc245ff2867f');
+
+    if (typeof result !== 'string') {
+      return result;
+    }
+    return null;
   }
   async getGolFlights(): Promise<IFlight[] | null> {
     const golFlightsUnsanitized = await this.getFlights('71f6629c-951c-4add-8c36-cf18bcd9d5ce');
-    let golFlightsSanitized: IFlight[] = [];
+    let golFlightsSanitized = [];
 
     if (typeof golFlightsUnsanitized === 'string') {
       golFlightsSanitized = Array(JSON.parse(golFlightsUnsanitized.replace('https://run.mocky.io/v3/faadd728-2e31-42f9-bf69-fc245ff2867f', '')));
@@ -26,8 +31,13 @@ class FlightsService {
     return golFlightsSanitized[0];
   }
 
-  async getAzulFlights() {
-    return await this.getFlights('916c6073-8d13-4464-a791-069bfebf41ed');
+  async getAzulFlights(): Promise<IFlight[] | null> {
+    const result = await this.getFlights('916c6073-8d13-4464-a791-069bfebf41ed');
+    
+    if (typeof result !== 'string') {
+      return result;
+    }
+    return null;
   }
 
 }
